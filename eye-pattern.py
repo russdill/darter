0b065c5786a72348602509efbc6ac974ddb320fe
Russ Dill <Russ.Dill@gmail.com>
2011-08-25 00:31:07 -0700

    Rewrite eye-pattern to be trigger centric.

--- eye-pattern.py
+++ eye-pattern.py
@@ -33,48 +33,106 @@ def parse_num(val):
 		e = 10**((e - 5) * 3)
 	return float(val) * e
 
+def get_vector(vectors, n):
+	return filter(lambda vect: vect.name == n,
+			vectors.get_datavectors())[0].get_data()
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--period', type=str, required=True,
-		help="Eye period")
+def plot_at(vector, idx, timestep, steps):
+	x_end = int(steps / 2)
+	x_begin = x_end - steps
+
+	begin = idx + x_begin
+	if begin < 0:
+		x_begin -= begin
+		begin = 0
+
+	end = idx + x_end
+	if end > len(vector):
+		x_end -= end - len(vector)
+		end = len(vector)
+
+	y = [ x * timestep for x in range(x_begin, x_end) ]
+	plot(y, vector[begin:end], color='blue')
+
+parser = argparse.ArgumentParser(conflict_handler='resolve')
+parser.add_argument('-w', '--width', type=str, required=True,
+	help="Plot width in seconds")
 parser.add_argument('-v', '--vector', type=str, required=True,
-		help="Spice vector to process")
-parser.add_argument('-s', '--start', default=0, type=str,
-		help='Start offset, not counting overlap (default: %(default)s)')
-parser.add_argument('-o', '--overlap', default=1.1, type=float,
-		help='Overlap factor (default: %(default)s)')
+	help="Spice vector to plot")
 parser.add_argument('input', type=argparse.FileType('rb'),
-		help='Spice3f5 input file')
+	help='Spice3f5 input file')
 parser.add_argument('output', type=argparse.FileType('wb'), nargs='?',
-		help='Plot output file')
+	help='Plot output file')
+parser.add_argument('-s', '--start', default='0', type=str,
+	help='First manual trigger (default: %(default)s)')
+parser.add_argument('-p', '--period', type=str,
+	help='Manual trigger period')
+parser.add_argument('-r', '--rising-trigger', type=str, nargs=2,
+	help="Auto rising trigger <vector> <voltage>")
+parser.add_argument('-f', '--falling-trigger', type=str, nargs=2,
+	help="Auto falling trigger <vector> <voltage>")
+parser.add_argument('-h', '--hysteresis', default='0', type=str,
+	help='Auto trigger voltage hysteresis (default: %(default)s)')
 
 args = parser.parse_args()
 vectors = spice_read.spice_read(args.input).get_plots()[0]
-vector = filter(lambda vect: vect.name == args.vector, vectors.get_datavectors())[0].get_data()
+vector = get_vector(vectors, args.vector)
 time = vectors.get_scalevector().get_data()
 timestep = time[1]
 end_time = time[-1] + timestep
 
-period = parse_num(args.period)
+width = parse_num(args.width)
+steps = int(width / timestep)
 start = parse_num(args.start)
-width = period * args.overlap
-pre = period * (args.overlap - 1) / 2
 
-curr = start - pre
-while curr < end_time:
-	first = int(curr / timestep)
-	last = int((curr + width) / timestep)
-	x_start = 0
-	x_last = last - first
-	if first < 0:
-		x_start -= first
-		first = 0
-	if last > len(vector):
-		x_last -= last - len(vector)
-		last = len(vector)
-	plot(time[x_start:x_last], vector[first:last], color='blue')
-	curr += period;
+xlim(-width / 2, width / 2)
 
+if args.period:
+	period = parse_num(args.period)
+	center = start
+	while center < end_time:
+		plot_at(vector, int(center / timestep), timestep, steps)
+		center += period
+else:
+	hyst = parse_num(args.hysteresis)
+	fvect = None
+	rvect = None
+	if args.falling_trigger:
+		fvect = get_vector(vectors, args.falling_trigger[0])
+		fv = parse_num(args.falling_trigger[1])
+		fstate = fvect[0] <= fv
+	if args.rising_trigger:
+		rvect = get_vector(vectors, args.rising_trigger[0])
+		rv = parse_num(args.rising_trigger[1])
+		rstate = rvect[0] >= rv
+	both = False
+	if len(fvect) and len(rvect):
+		both = args.rising_trigger[0] == args.falling_trigger[0]
+	idx = 0
+	while idx < len(time):
+		curr = int(idx + start / timestep)
+		if both:
+			if (rstate and fvect[idx] <= fv) or (not rstate and rvect[idx] >= rv):
+				rstate = not rstate
+				plot_at(vector, curr, timestep, steps)
+		if len(fvect) and not both:
+			print fstate, fv, vector[idx]
+			if fstate and fvect[idx] > fv + hyst:
+				fstate = False
+			elif not fstate and fvect[idx] <= fv:
+				fstate = True
+				plot_at(vector, curr, timestep, steps)
+		if len(rvect) and not both:
+			print rstate, rv, rvect[idx]
+			if rstate and vector[idx] < rv - hyst:
+				rstate = False
+			elif not rstate and rvect[idx] >= rv:
+				rstate = True
+				plot_at(vector, curr, timestep, steps)
+		idx += 1
+
+
+title(args.vector)
 ylabel('Volts')
 xlabel('Time (s)')
 
